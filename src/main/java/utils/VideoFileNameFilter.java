f43f7e418f7c7500c5150d6c82640695392d5e71
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-17 16:07:37 +0300

    Added video title to VideoFileNameFilter
    Now VideoFileNameFilter checks only for a specific file.

--- src/main/java/utils/VideoFileNameFilter.java
+++ src/main/java/utils/VideoFileNameFilter.java
@@ -6,12 +6,18 @@ import java.io.FilenameFilter;
 
 public class VideoFileNameFilter implements FilenameFilter {
 
+    private final String videoTitle;
+
+    public VideoFileNameFilter(String videoTitle) {
+        this.videoTitle = videoTitle;
+    }
+
     @Override
     // Checks every combination of container and resolution for one filename
     public boolean accept(File dir, String name) {
         for (Integer res : VideoInfo.getResolutions()) {
             for (String container : VideoInfo.getContainers()) {
-                if(name.equals(name + "-" + String.valueOf(res) + "." + container)) {
+                if(name.equals(videoTitle + "-" + String.valueOf(res) + "p." + container)) {
                     return true;
                 }
             }
