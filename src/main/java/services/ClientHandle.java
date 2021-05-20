d1e3e5ec02130a3d306b3e3ff3227efe085cbcd0
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-20 18:09:29 +0300

    Implemented ServersSocket and client handle class

---
+++ src/main/java/services/ClientHandle.java
@@ -0,0 +1,45 @@
+package services;
+
+import res.MediaInfo;
+import res.VideoInfo;
+import streaming.StreamingServerSocket;
+
+import javax.print.attribute.standard.Media;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ClientHandle implements Runnable {
+    private final StreamingServerSocket server;
+    private final Socket socket;
+
+    public ClientHandle(StreamingServerSocket server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    private HashMap<String, ArrayList<Integer>> getSupportedFiles(ArrayList<Integer> supportedResolutions, String format) {
+        HashMap<String, ArrayList<Integer>> supportedFiles = new HashMap<String, ArrayList<Integer>>();
+
+        for (VideoInfo video : server.getVideoList()) {
+            supportedFiles.put(video.getTitle(), video.getSupportedResolutions(supportedResolutions, format));
+        }
+
+        return supportedFiles;
+    }
+
+    @Override
+    public void run() {
+        ObjectInputStream input = null;
+        ObjectOutputStream output = null;
+        try {
+            input = new ObjectInputStream(socket.getInputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/streaming/StreamingServerSocket.java
@@ -0,0 +1,48 @@
+package streaming;
+
+import res.VideoInfo;
+import services.ClientHandle;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class StreamingServerSocket {
+    private ServerSocket server = null;
+    private final int port;
+    private final ArrayList<VideoInfo> videosInfo;
+
+    public StreamingServerSocket(int port, ArrayList<VideoInfo> videosInfo) {
+        this.port = port;
+        this.videosInfo = videosInfo;
+    }
+    public StreamingServerSocket(ArrayList<VideoInfo> videosInfo) {
+        this(5000, videosInfo);
+    }
+
+    public ArrayList<VideoInfo> getVideoList() {
+        return videosInfo;
+    }
+
+    public void start() {
+        try {
+            server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            Socket clientSocket = null;
+
+            try {
+                clientSocket = server.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            new Thread(new ClientHandle(this, clientSocket)).start();
+        }
+
+    }
+}
