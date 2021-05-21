26b68e968a9debe06302ec25e87c1d8cbe342aa4
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-21 22:14:48 +0300

    Fixes FileServer
    Now the file will be sent correctly by utilizing DataOutputStream instead of ObjectOutputStream. The later will only be used to send the file name and the size of it

--- src/main/java/services/FileServer.java
+++ src/main/java/services/FileServer.java
@@ -1,24 +1,32 @@
 package services;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileServer {
     private final String filePath;
-    private final OutputStream fileStream;
+    private final ObjectOutputStream objectOutputStream;
+    private final OutputStream socketStream;
 
-    public FileServer(String filePath, OutputStream fileStream) {
+    public FileServer(String filePath, ObjectOutputStream objectOutputStream, OutputStream socketStream) {
         this.filePath = filePath;
-        this.fileStream = fileStream;
+        this.objectOutputStream = objectOutputStream;
+        this.socketStream = socketStream;
     }
 
     public void transmit() {
         File rdp = new File(filePath);
+
         byte[] fileBytes = new byte[(int) rdp.length()];
 
         BufferedInputStream fileBuff = null;
+        DataOutputStream fileStream = null;
         try {
-            fileBuff = new BufferedInputStream(new FileInputStream(rdp));
+            fileBuff = new BufferedInputStream(new FileInputStream(filePath));
             fileBuff.read(fileBytes, 0, fileBytes.length);
+
+            fileStream = new DataOutputStream(socketStream);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -26,13 +34,16 @@ public class FileServer {
         }
 
         try {
+            // First send the name and size of the file
+            // and after that the file itself.
+            int pos = filePath.lastIndexOf('\\') + 1;
+            objectOutputStream.writeObject(filePath.substring(pos, filePath.length()) + "#" + fileBytes.length);
             fileStream.write(fileBytes, 0, fileBytes.length);
         } catch (IOException e) {
             e.printStackTrace();
         }
         finally {
             try {
-                fileStream.close();
                 fileBuff.close();
             } catch (IOException e) {
                 e.printStackTrace();
