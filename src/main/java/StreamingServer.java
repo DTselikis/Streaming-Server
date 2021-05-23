1f905bb5c76f5b12559e4aacb5c585be0f8ebfe9
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2021-05-23 13:58:35 +0300

    Implemented loggint to TextArea
    Updated GUI by adding a TextArea for logging

--- src/main/java/StreamingServer.java
+++ src/main/java/StreamingServer.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import utils.TextAreaAppender;
 
 public class StreamingServer extends Application {
     private static final Logger LOGGER = LogManager.getLogger(StreamingServer.class);
@@ -12,6 +14,8 @@ public class StreamingServer extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("StreamingServer.fxml"));
         Scene scene = new Scene(loader.load());
+        TextAreaAppender.textArea = (TextArea) scene.lookup("#loggingTA");
+
         stage.setScene(scene);
         stage.show();
     }
